fa8d1fa2ddb8a6a295fb87c2c020f66c48e040c2
Dillon Waugh <ekove@Dillons-MacBook-Air.local>
2022-03-17 06:59:48 -0500

    updated procfile

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://wapitwcdmdnjgu:68fe61de4936550f9bd818233533d7c067a9227ce13286f2e61726dffc909741@ec2-54-85-113-73.compute-1.amazonaws.com:5432/d8qhlqiumuieep'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER='app/static/uploads'
 
